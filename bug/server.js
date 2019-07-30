daae7880db71a9853a741d07a401d46536c2951a
Clark Alms <clark.alms@gmail.com>
2019-07-30 17:19:38 -0500

    added basic file structure

---
+++ bug/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
